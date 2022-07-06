ce0e03cde4ea81a964f64f252244cbe82d69dcc7
Kausalya R <iamk222000@gmail.com>
2022-07-06 20:06:10 +0530

    Hello World Commit

---
+++ src/main/java/com/tgt/rysetti/learningresourcsesapi/LearningresourcsesapiApplication.java
@@ -0,0 +1,14 @@
+package com.tgt.rysetti.learningresourcsesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcsesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcsesapiApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
